abe8120f02a2b47a808adfd7d44084ac9c1db3c0
Safayet Jamil <work.jamil4@gmail.com>
2021-12-10 00:42:48 +0600

    Jamil Job Management
    	1:Downgrade django version to 3.2

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from jobmanagers import views
-from django.conf.urls import url
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
