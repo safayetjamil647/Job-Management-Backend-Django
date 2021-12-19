1cd5bb69631dd62e6ddd09cb95125c6db213f1e1
Safayet Jamil <work.jamil4@gmail.com>
2021-12-19 17:35:54 +0600

    job-manager
    	1:template integration for admin dashboard

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 from rest_framework import routers
 from jobmanagers import views
 
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
@@ -26,8 +27,10 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('jobdata/', include('jobmanagers.urls')),
+    path('',view=views.HomePageView.as_view(),name='home'),
+    path('about/',view=views.AboutPageView.as_view(),name='about') 
 ]
