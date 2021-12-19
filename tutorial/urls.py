eaa07b865553a9fb0654e3777a46869d2687e4a6
Safayet Jamil <work.jamil4@gmail.com>
2021-12-19 21:28:17 +0600

    job-manager
    	1:dashboard layout design with css and responsive

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
@@ -33,4 +35,4 @@ urlpatterns = [
     path('jobdata/', include('jobmanagers.urls')),
     path('',view=views.HomePageView.as_view(),name='home'),
     path('about/',view=views.AboutPageView.as_view(),name='about') 
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
